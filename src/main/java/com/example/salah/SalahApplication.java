babd4b790b2ea5082253cb289146bddba4f3e43e
SamuelPaulos <samuel.paulos@yahoo.com>
2019-04-28 17:52:00 -0400

    first commit

---
+++ src/main/java/com/example/salah/SalahApplication.java
@@ -0,0 +1,13 @@
+package com.example.salah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalahApplication.class, args);
+	}
+
+}
